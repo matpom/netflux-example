48ad558f73e0f5517a695880f837646c1fa404f5
Mateusz Pomorski <pomorski.mateusz@gmail.com>
2018-03-25 14:23:01 +0100

    Initial project setup

---
+++ src/main/java/us/pomorscy/spring/reactive/netflux/NetfluxApplication.java
@@ -0,0 +1,12 @@
+package us.pomorscy.spring.reactive.netflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NetfluxApplication.class, args);
+  }
+}
